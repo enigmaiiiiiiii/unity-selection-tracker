b6e93180903125eb76e0ec788cbd4af57ad0be1c
enigmaiiiiii <enigmaiiiiii@localhost.localdomain>
2025-02-19 11:36:28 +0800

    update

--- Editor/UI/UIAssetManager.cs
+++ Editor/UI/UIAssetManager.cs
@@ -3,9 +3,7 @@ using UnityEngine.UIElements;
 
 namespace Synaptafin.Editor.SelectionTracker {
 
-  [CreateAssetMenu(fileName = "UIAssetManager", menuName = "SO/UIAssetManager")]
   public class UIAssetManager : ScriptableObject {
-
     public StyleSheet globalStyle;
     public VisualTreeAsset preferenceTemplate;
     public VisualTreeAsset TrackerTemplate;
